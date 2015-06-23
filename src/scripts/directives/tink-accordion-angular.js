45dd1d80ec6359e56c2b67b9572b5a8367bb6160
vincent bouillart <vincent.bouillart@appfoundry.be>
2015-06-23 11:15:47 +0200

    compile de header

--- src/scripts/directives/tink-accordion-angular.js
+++ src/scripts/directives/tink-accordion-angular.js
@@ -46,7 +46,7 @@
       }
     };
   })
-  .directive('tinkAccordionPanel', function() {
+  .directive('tinkAccordionPanel', function($compile) {
     return {
       require:'^tinkAccordion',         // We need this directive to be inside an accordion group
       restrict:'EA',
@@ -72,7 +72,9 @@
           }else{
             element.find('.panel-title').html(scope.heading);
             element.find('.accordion-loaded-content').append(clone);
+            $compile(element)(scope.$parent);
           }
+
         },scope);
 
        var states = {closed:1,open:2,loading:0};
